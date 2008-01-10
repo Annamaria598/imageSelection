a233c70930fad28c7a7ac836dc59f155cdec4ae9
Annamaria598 <annamaria.dambrosio2@studio.unibo.it>
2008-01-10 02:36:05 +0100

    aggiunti filtro varianza e filtro entropia

--- classificazione_knn.py
+++ classificazione_knn.py
@@ -3,7 +3,22 @@ import numpy as np
 import scipy.ndimage as nd 
 import sklearn
 from skimage.color import rgb2lab, rgba2rgb, rgb2hsv
+from scipy.stats  import entropy
 
+def _entropy(values):
+    probabilities = np.bincount(values.astype(np.int)) / float(len(values))
+    return entropy(probabilities)
+
+def local_entropy(img, kernel_radius=2):
+
+    """
+    Calcolo dell'entropia per l'intorno di ogni pixel in un intorno specificato del kernel.
+
+    img: ndarray dell'immagine.
+    kernel_radius : int, dimensione del kernel 
+    """
+
+    return nd.generic_filter(img, _entropy, size = kernel_radius)
 
 def image_to_data(img, size = 13):
     """
@@ -48,6 +63,22 @@ def image_to_data(img, size = 13):
     G_edge = nd.sobel(G)
     B_edge = nd.sobel(B)
 
+    # varianza
+    mean_R = nd.uniform_filter(R, (size, size))
+    mean_G = nd.uniform_filter(G, (size, size))
+    mean_B = nd.uniform_filter(B, (size, size))
+    mean_sqr_R = nd.uniform_filter(R**2, (size,size))
+    mean_sqr_G = nd.uniform_filter(G**2, (size,size))
+    mean_sqr_B = nd.uniform_filter(B**2, (size,size))
+
+    R_variance = mean_sqr_R - mean_R**2
+    G_variance = mean_sqr_G - mean_G**2
+    B_variance = mean_sqr_B - mean_B**2
+
+    #entropia
+    R_entropy = local_entropy(R, size)
+    G_entropy = local_entropy(G, size)
+    B_entropy = local_entropy(B, size)
 
 # ----- SPAZIO COLORE LAB -----
    
@@ -78,8 +109,23 @@ def image_to_data(img, size = 13):
     a_edge = nd.sobel(a)
     b_edge = nd.sobel (b)
 
+    
     # varianza
-    # in attesa di chiarimenti
+    mean_L = nd.uniform_filter(L, (size, size))
+    mean_a = nd.uniform_filter(a, (size, size))
+    mean_b = nd.uniform_filter(b, (size, size))
+    mean_sqr_L = nd.uniform_filter(L**2, (size,size))
+    mean_sqr_a = nd.uniform_filter(a**2, (size,size))
+    mean_sqr_b = nd.uniform_filter(b**2, (size,size))
+
+    L_variance = mean_sqr_L - mean_L**2
+    a_variance = mean_sqr_a - mean_a**2
+    b_variance = mean_sqr_b - mean_b**2
+
+     #entropia
+    L_entropy = local_entropy(L, size)
+    a_entropy = local_entropy(a, size)
+    b_entropy = local_entropy(b, size)
 
 # ----- SPAZIO COLORE HSV -----
    
@@ -111,14 +157,33 @@ def image_to_data(img, size = 13):
     s_edge = nd.sobel(s)
     v_edge = nd.sobel(v)
 
+    # varianza
+    mean_h = nd.uniform_filter(h, (size, size))
+    mean_s = nd.uniform_filter(s, (size, size))
+    mean_v = nd.uniform_filter(v, (size, size))
+    mean_sqr_h = nd.uniform_filter(h**2, (size,size))
+    mean_sqr_s = nd.uniform_filter(s**2, (size,size))
+    mean_sqr_v = nd.uniform_filter(v**2, (size,size))
+
+    h_variance = mean_sqr_h - mean_h**2
+    s_variance = mean_sqr_s - mean_s**2
+    v_variance = mean_sqr_v - mean_v**2
+
+     #entropia
+    h_entropy = local_entropy(h, size)
+    s_entropy = local_entropy(s, size)
+    v_entropy = local_entropy(v, size)
+
     
         
 
     # Organizziamo i dati per l'utilizzo con algoritmi di machine learning
 
-    layers = [L,a,b, L_mf, a_mf, b_mf, L_edge, a_edge, b_edge, \
-        R, G, B, R_mf, G_mf, B_mf, R_edge, G_edge, B_edge, \
-        h, s, v, h_mf, s_mf, v_mf, h_edge, s_edge, v_edge]
+    layers = [L,a,b, L_mf, a_mf, b_mf, L_edge, a_edge, b_edge, L_variance, a_variance, b_variance, L_entropy, a_variance, b_variance,\
+        R, G, B, R_mf, G_mf, B_mf, R_edge, G_edge, B_edge, R_variance, G_variance, B_variance, R_entropy, G_entropy, B_entropy, \
+        h, s, v, h_mf, s_mf, v_mf, h_edge, s_edge, v_edge, h_variance, s_variance, v_variance, h_entropy, s_entropy, v_entropy]
+
+    
 
     # Unisco i vettori di features in una matrice con una riga per featur ed una colonna per pixel
     data= np.stack([l.ravel() for l in layers])
