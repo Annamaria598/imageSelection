e0bbdff9b89643a84cc2a9ac1605090b34afd227
Annamaria598 <annamaria.dambrosio2@studio.unibo.it>
2020-12-12 12:41:04 +0100

    Prima prova Knn

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Classificazione con KNN
+Nel notebook Knn_prova.ipynb è possibile vedere una prima prova 
+di training e scoring di un modello Knn per la segmentazione
+su immagini di pelle.
+
+Tutte le funzioni di supporto sono disponibili nel file
+classificazione_knn.py
+
+
 # ImageSelection
 
 Questo è uno strumento per separare porzioni di immagini 
--- classificazione_knn.py
+++ classificazione_knn.py
@@ -2,27 +2,55 @@ import imageio
 import numpy as np
 import scipy.ndimage as nd 
 import sklearn
-from skimage.color import rgb2lab, rgba2rgb
+from skimage.color import rgb2lab, rgba2rgb, rgb2hsv
 
 
-def image_to_data(img):
+def image_to_data(img, size = 13):
     """
     Genera e associa attributi ad ogni pixel dell'immagine utilizzando vari filtri.
 
     img: str, il path ad un'immagine.
+    size : int, la dimensione della finestra da utilizzare per i filtri
     """
 
     # apriamo immagine come matrice in RGB
     image_array = imageio.imread(img)
 
-    #convertiamo in RGB in LAB
-    # se l'immagine aperta era un PNG abbiamo rgba
-    # invece di rgb ma rgb2lab vuole solo i tre canali
-    # del colore
+# ----- SPAZIO COLORE RGB ----
 
     if(img.split(".")[-1] in ["png", "PNG"]):
         image_array = rgba2rgb(image_array)
 
+    
+    #separiamo le componenti R,G,B
+    R = image_array[..., 0]
+    G = image_array[..., 1]
+    B = image_array[..., 2]
+
+    #normalizziamo i valori per stare attorno all'intervallo -0.5, 0.5
+    R = R / 255
+    R = R - 0.5
+    G = G / 255
+    G = G - 0.5
+    B = B / 255
+    B = B - 0.5
+
+    # calcolo filtro mediano
+    R_mf = nd.median_filter (R, size =size)
+    G_mf = nd.median_filter (G, size = size)
+    B_mf = nd.median_filter (B, size = size)
+
+
+    # edge detection
+    # usiamo per il momento i parametri di default e ci riserviamo in futuro la possibilità di 
+    # sperimentare con parametri diversi
+    R_edge = nd.sobel(R)
+    G_edge = nd.sobel(G)
+    B_edge = nd.sobel(B)
+
+
+# ----- SPAZIO COLORE LAB -----
+   
     image_array_lab = rgb2lab(image_array)
 
     #separiamo le componenti L, a, b
@@ -37,12 +65,10 @@ def image_to_data(img):
     b = b / 50
 
     # calcolo filtro mediano
-    # scegliamo per iniziare una finestra di dimensione 13, ma nulla ci impedisce più avanti di 
-    # sperimentare con valori differenti.
-    size = 13
-    L_mf_13 = nd.median_filter (L, size =size)
-    a_mf_13 = nd.median_filter (a, size = size)
-    b_mf_13 = nd.median_filter (b, size = size)
+    
+    L_mf = nd.median_filter (L, size =size)
+    a_mf = nd.median_filter (a, size = size)
+    b_mf = nd.median_filter (b, size = size)
 
 
     # edge detection
@@ -52,27 +78,47 @@ def image_to_data(img):
     a_edge = nd.sobel(a)
     b_edge = nd.sobel (b)
 
-    # covarianza
+    # varianza
     # in attesa di chiarimenti
 
+# ----- SPAZIO COLORE HSV -----
+   
+    image_array_hsv = rgb2hsv(image_array)
+
+    #separiamo le componenti H, S, V
+    h = image_array_lab[..., 0]
+    s = image_array_lab[..., 1]
+    v = image_array_lab[..., 2]
+
+    #normalizziamo i valori per stare attorno all'intervallo -0.5, 0.5
+    h = h / 360
+    h = h - 0.5
+    s = s - 0.5
+    v = v - 0.5
+   
+
+    # calcolo filtro mediano
     
-    features_dict = {
-        "img": image_array_lab,
-        "L" : L,
-        "a" : a,
-        "b" : b,
-        "L_mf": L_mf_13,
-        "a_mf": a_mf_13,
-        "b_mf": b_mf_13,
-        "L_edge" : L_edge,
-        "a_edge" : a_edge,
-        "b_edge" : b_edge
-    }
+    h_mf = nd.median_filter (h, size =size)
+    s_mf = nd.median_filter (s, size = size)
+    v_mf = nd.median_filter (v, size = size)
+
+
+    # edge detection
+    # usiamo per il momento i parametri di default e ci riserviamo in futuro la possibilità di 
+    # sperimentare con parametri diversi
+    h_edge = nd.sobel(h)
+    s_edge = nd.sobel(s)
+    v_edge = nd.sobel(v)
+
     
+        
 
     # Organizziamo i dati per l'utilizzo con algoritmi di machine learning
 
-    layers = [L,a,b, L_mf_13, a_mf_13, b_mf_13, L_edge, a_edge, b_edge]
+    layers = [L,a,b, L_mf, a_mf, b_mf, L_edge, a_edge, b_edge, \
+        R, G, B, R_mf, G_mf, B_mf, R_edge, G_edge, B_edge, \
+        h, s, v, h_mf, s_mf, v_mf, h_edge, s_edge, v_edge]
 
     # Unisco i vettori di features in una matrice con una riga per featur ed una colonna per pixel
     data= np.stack([l.ravel() for l in layers])
@@ -80,3 +126,40 @@ def image_to_data(img):
     # Traspongo perchè voglio colonne per features e righe per pixel
     data = data.T
     return data
+
+def pixel_to_class(pixel):
+    """
+    Assegna ad un singolo pixel una label intera in base al colore di cui è stato colorato
+    """
+    
+    if (pixel == [255,255,255,255]).all(): # pixel del vetrino
+        return 0
+    elif (pixel == [163,167,69,255]).all(): #strato corneo
+        return 1
+    elif(pixel == [0,255,255,255]).all(): # derma
+        return 2
+    elif(pixel == [25, 55, 190, 255]).all(): # pixel dell'epidermide
+        return 3
+    else: #pixel dei vasi
+        return 4
+
+
+def colimage_to_classes(img):
+    """
+    Crea le classi per ogni pixel a partire da un'immagine colorata.
+    
+    img: str, uri dell'immagine colorata.
+    """
+
+    img_col = imageio.imread(img)
+
+    classes_matrix = []
+    
+
+    for l in range(len(img_col)):
+        for c in range (len(img_col[0])):
+            classes_matrix.append(pixel_to_class(img_col[l][c]))
+
+    classes_matrix = np.array(classes_matrix)
+
+    return classes_matrix
