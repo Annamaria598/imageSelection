2cb4ca7208525288c3f4878e7417d95a9a5e97e0
Annamaria598 <annamaria.dambrosio2@studio.unibo.it>
2021-01-05 14:01:12 +0100

    script per training su server

--- classificazione_knn.py
+++ classificazione_knn.py
@@ -1,243 +1,238 @@
-import imageio 
+import imageio
 import numpy as np
-import scipy.ndimage as nd 
+import scipy.ndimage as nd
 import sklearn
 from skimage.color import rgb2lab, rgba2rgb, rgb2hsv
-from scipy.stats  import entropy
+from scipy.stats import entropy
+from sklearn.metrics import matthews_corrcoef
 
 def _entropy(values):
     probabilities = np.bincount(values.astype(np.int)) / float(len(values))
     return entropy(probabilities)
 
 def local_entropy(img, kernel_radius=2):
-
     """
-    Calcolo dell'entropia per l'intorno di ogni pixel in un intorno specificato del kernel.
+    Calcolo dell'entropia per l'intorno di ogni pixel in
+    un intorno specificato dal kernel.
 
     img: ndarray dell'immagine.
-    kernel_radius : int, dimensione del kernel 
+    kernel_radius: int, dimensione del kernel
     """
+    return nd.generic_filter(img, _entropy, size=kernel_radius)
 
-    return nd.generic_filter(img, _entropy, size = kernel_radius)
 
-def image_to_data(img, size = 13):
+def image_to_data(img, size=13):
     """
-    Genera e associa attributi ad ogni pixel dell'immagine utilizzando vari filtri.
+    Genera ed associa attributi ad ogni pixel dell'immagine
+    utilizzando vari filtri.
+
+    Restituisce un dizionario di matrici. Ogni matrice è una feature/livello.
 
     img: str, il path ad un'immagine.
-    size : int, la dimensione della finestra da utilizzare per i filtri
+    size: int, la dimensione della finestra da utilizzare per i filtri.
     """
+
+    # mettiamo tutti i layer/features in un dizionario
+    layers = {}
 
     # apriamo immagine come matrice in RGB
     image_array = imageio.imread(img)
 
-# ----- SPAZIO COLORE RGB ----
-
-    if(img.split(".")[-1] in ["png", "PNG"]):
+    # ----- SPAZIO COLORE RGB -----
+    if (img.split(".")[-1] in ["png", "PNG"]):
         image_array = rgba2rgb(image_array)
 
-    
-    #separiamo le componenti R,G,B
+    # separiamo le componenti R, G, B
     R = image_array[..., 0]
     G = image_array[..., 1]
     B = image_array[..., 2]
 
-    #normalizziamo i valori per stare attorno all'intervallo -0.5, 0.5
-    R = R / 255
-    R = R - 0.5
-    G = G / 255
-    G = G - 0.5
-    B = B / 255
-    B = B - 0.5
+    # normalizziamo i valori per restare
+    # attorno all'intervallo -0.5, 0.5
+    # Se abbiamo usato rgba2rgb non è necessario
+    # dividere per 255
+    #R = R / 255
+    layers['R'] = R - 0.5
+    #G = G / 255
+    layers['G'] = G - 0.5
+    #B = B / 255
+    layers['B'] = B - 0.5
 
     # calcolo filtro mediano
-    R_mf = nd.median_filter (R, size =size)
-    G_mf = nd.median_filter (G, size = size)
-    B_mf = nd.median_filter (B, size = size)
-
+    layers['R_mf'] = nd.median_filter(R, size=size)
+    layers['G_mf'] = nd.median_filter(G, size=size)
+    layers['B_mf'] = nd.median_filter(B, size=size)
 
     # edge detection
-    # usiamo per il momento i parametri di default e ci riserviamo in futuro la possibilità di 
+    # usiamo per il momento i parametri di default
+    # e ci riserviamo in futuro la possibilità di
     # sperimentare con parametri diversi
-    R_edge = nd.sobel(R)
-    G_edge = nd.sobel(G)
-    B_edge = nd.sobel(B)
+    layers['R_edge'] = nd.sobel(R)
+    layers['G_edge'] = nd.sobel(G)
+    layers['B_edge'] = nd.sobel(B)
 
     # varianza
     mean_R = nd.uniform_filter(R, (size, size))
     mean_G = nd.uniform_filter(G, (size, size))
     mean_B = nd.uniform_filter(B, (size, size))
-    mean_sqr_R = nd.uniform_filter(R**2, (size,size))
-    mean_sqr_G = nd.uniform_filter(G**2, (size,size))
-    mean_sqr_B = nd.uniform_filter(B**2, (size,size))
+    mean_sqr_R = nd.uniform_filter(R**2, (size, size))
+    mean_sqr_G = nd.uniform_filter(G**2, (size, size))
+    mean_sqr_B = nd.uniform_filter(B**2, (size, size))
 
-    R_variance = mean_sqr_R - mean_R**2
-    G_variance = mean_sqr_G - mean_G**2
-    B_variance = mean_sqr_B - mean_B**2
+    layers['R_variance'] = mean_sqr_R - mean_R**2
+    layers['G_variance'] = mean_sqr_G - mean_G**2
+    layers['B_variance'] = mean_sqr_B - mean_B**2
 
-    #entropia
-    R_entropy = local_entropy(R, size)
-    G_entropy = local_entropy(G, size)
-    B_entropy = local_entropy(B, size)
+    # ----- SPAZIO COLORE LAB -----
 
-# ----- SPAZIO COLORE LAB -----
-   
     image_array_lab = rgb2lab(image_array)
 
-    #separiamo le componenti L, a, b
+    # separiamo le componenti L, a , b
     L = image_array_lab[..., 0]
     a = image_array_lab[..., 1]
     b = image_array_lab[..., 2]
 
-    #normalizziamo i valori per stare attorno all'intervallo -0.5, 0.5
+    # Sfruttiamo la componente L per calcolare
+    # un filtro entropia
+    layers["entropy"] = local_entropy(L, kernel_radius=size)
+
+    # Normalizzo l'entropia dividendo per il massimo valore
+    # di entropia possibile su un segnale lungo size*size
+    # con 100 simboli a disposizione
+    # questo calcolo del massimo per cui dividere è valido solo
+    # per una finestra con un massimo di 200 pixel
+    signal_size = size**2
+    if signal_size > 100:
+        max_ent = entropy(list(range(100)) + [i for i in range(signal_size - 100)])
+    else:
+        max_ent = entropy(list(range(signal_size)))
+    layers["entropy"] = ( layers["entropy"] / max_ent ) - 0.5
+
+    # normalizziamo i valori per restare
+    # attorno all'intervallo -0.5, 0.5
+    # L va da 0 a 100
+    # a e b vanno da -120 a +120
     L = L / 100
-    L = L - 0.5
-    a = a / 50
-    b = b / 50
+    layers['L'] = L - 0.5
+    layers['a'] = a / 240
+    layers['b'] = b / 240
 
     # calcolo filtro mediano
-    
-    L_mf = nd.median_filter (L, size =size)
-    a_mf = nd.median_filter (a, size = size)
-    b_mf = nd.median_filter (b, size = size)
-
+    layers['L_mf'] = nd.median_filter(L, size=size)
+    layers['a_mf'] = nd.median_filter(a, size=size)
+    layers['b_mf'] = nd.median_filter(b, size=size)
 
     # edge detection
-    # usiamo per il momento i parametri di default e ci riserviamo in futuro la possibilità di 
+    # usiamo per il momento i parametri di default
+    # e ci riserviamo in futuro la possibilità di
     # sperimentare con parametri diversi
-    L_edge = nd.sobel(L)
-    a_edge = nd.sobel(a)
-    b_edge = nd.sobel (b)
+    layers['L_edge'] = nd.sobel(L)
+    layers['a_edge'] = nd.sobel(a)
+    layers['b_edge'] = nd.sobel(b)
 
-    
     # varianza
     mean_L = nd.uniform_filter(L, (size, size))
     mean_a = nd.uniform_filter(a, (size, size))
     mean_b = nd.uniform_filter(b, (size, size))
-    mean_sqr_L = nd.uniform_filter(L**2, (size,size))
-    mean_sqr_a = nd.uniform_filter(a**2, (size,size))
-    mean_sqr_b = nd.uniform_filter(b**2, (size,size))
+    mean_sqr_L = nd.uniform_filter(L**2, (size, size))
+    mean_sqr_a = nd.uniform_filter(a**2, (size, size))
+    mean_sqr_b = nd.uniform_filter(b**2, (size, size))
 
-    L_variance = mean_sqr_L - mean_L**2
-    a_variance = mean_sqr_a - mean_a**2
-    b_variance = mean_sqr_b - mean_b**2
+    layers['L_variance'] = mean_sqr_L - mean_L**2
+    layers['a_variance'] = mean_sqr_a - mean_a**2
+    layers['b_variance'] = mean_sqr_b - mean_b**2
 
-     #entropia
-    L_entropy = local_entropy(L, size)
-    a_entropy = local_entropy(a, size)
-    b_entropy = local_entropy(b, size)
+    # ----- SPAZIO COLORE HSV -----
 
-# ----- SPAZIO COLORE HSV -----
-   
     image_array_hsv = rgb2hsv(image_array)
 
-    #separiamo le componenti H, S, V
-    h = image_array_lab[..., 0]
-    s = image_array_lab[..., 1]
-    v = image_array_lab[..., 2]
+    # separiamo le componenti H, S, V
+    h = image_array_hsv[..., 0]
+    s = image_array_hsv[..., 1]
+    v = image_array_hsv[..., 2]
 
-    #normalizziamo i valori per stare attorno all'intervallo -0.5, 0.5
+    # normalizziamo i valori per restare
+    # attorno all'intervallo -0.5, 0.5
     h = h / 360
-    h = h - 0.5
-    s = s - 0.5
-    v = v - 0.5
-   
+    layers['h'] = h - 0.5
+    layers['s'] = s - 0.5
+    layers['v'] = v - 0.5
 
     # calcolo filtro mediano
-    
-    h_mf = nd.median_filter (h, size =size)
-    s_mf = nd.median_filter (s, size = size)
-    v_mf = nd.median_filter (v, size = size)
-
+    layers['h_mf'] = nd.median_filter(h, size=size)
+    layers['s_mf'] = nd.median_filter(s, size=size)
+    layers['v_mf'] = nd.median_filter(v, size=size)
 
     # edge detection
-    # usiamo per il momento i parametri di default e ci riserviamo in futuro la possibilità di 
+    # usiamo per il momento i parametri di default
+    # e ci riserviamo in futuro la possibilità di
     # sperimentare con parametri diversi
-    h_edge = nd.sobel(h)
-    s_edge = nd.sobel(s)
-    v_edge = nd.sobel(v)
+    layers['h_edge'] = nd.sobel(h)
+    layers['s_edge'] = nd.sobel(s)
+    layers['v_edge'] = nd.sobel(v)
 
     # varianza
     mean_h = nd.uniform_filter(h, (size, size))
     mean_s = nd.uniform_filter(s, (size, size))
     mean_v = nd.uniform_filter(v, (size, size))
-    mean_sqr_h = nd.uniform_filter(h**2, (size,size))
-    mean_sqr_s = nd.uniform_filter(s**2, (size,size))
-    mean_sqr_v = nd.uniform_filter(v**2, (size,size))
+    mean_sqr_h = nd.uniform_filter(h**2, (size, size))
+    mean_sqr_s = nd.uniform_filter(s**2, (size, size))
+    mean_sqr_v = nd.uniform_filter(v**2, (size, size))
 
-    h_variance = mean_sqr_h - mean_h**2
-    s_variance = mean_sqr_s - mean_s**2
-    v_variance = mean_sqr_v - mean_v**2
+    layers['h_variance'] = mean_sqr_h - mean_h**2
+    layers['s_variance'] = mean_sqr_s - mean_s**2
+    layers['v_variance'] = mean_sqr_v - mean_v**2
 
-     #entropia
-    h_entropy = local_entropy(h, size)
-    s_entropy = local_entropy(s, size)
-    v_entropy = local_entropy(v, size)
+    return layers
 
-    
-        
-
-    # Organizziamo i dati per l'utilizzo con algoritmi di machine learning
-
-    layers = [L,a,b, L_mf, a_mf, b_mf, L_edge, a_edge, b_edge, L_variance, a_variance, b_variance, L_entropy, a_variance, b_variance,\
-        R, G, B, R_mf, G_mf, B_mf, R_edge, G_edge, B_edge, R_variance, G_variance, B_variance, R_entropy, G_entropy, B_entropy, \
-        h, s, v, h_mf, s_mf, v_mf, h_edge, s_edge, v_edge, h_variance, s_variance, v_variance, h_entropy, s_entropy, v_entropy]
-
-    
-
-    # Unisco i vettori di features in una matrice con una riga per featur ed una colonna per pixel
-    data= np.stack([l.ravel() for l in layers])
-
-    # Traspongo perchè voglio colonne per features e righe per pixel
-    data = data.T
-    return data
 
 def pixel_to_class(pixel):
     """
-    Assegna ad un singolo pixel una label intera in base al colore di cui è stato colorato
+    Assegna ad un singolo pixel una label intera in base al
+    colore di cui è stato colorato.
     """
-    
-    if (pixel == [255,255,255,255]).all(): # pixel del vetrino
+
+    if (pixel == [255, 255, 255, 255]).all(): # pixel del vetrino
         return 0
-    elif (pixel == [163,167,69,255]).all(): #strato corneo
+    elif (pixel == [163, 167, 69, 255]).all(): # pixel dello strato corneo
         return 1
-    elif(pixel == [0,255,255,255]).all(): # derma
+    elif (pixel == [0, 255, 255, 255]).all(): # pixel del derma
         return 2
-    elif(pixel == [25, 55, 190, 255]).all(): # pixel dell'epidermide
+    elif (pixel == [25, 55, 190, 255]).all(): # pixel dell'epidermide
         return 3
-    else: #pixel dei vasi
+    else: # pixel dei vasi
         return 4
 
 
 def colimage_to_classes(img):
     """
     Crea le classi per ogni pixel a partire da un'immagine colorata.
-    
+
     img: str, uri dell'immagine colorata.
     """
 
     img_col = imageio.imread(img)
 
     classes_matrix = []
-    
 
     for l in range(len(img_col)):
-        for c in range (len(img_col[0])):
+        for c in range(len(img_col[0])):
             classes_matrix.append(pixel_to_class(img_col[l][c]))
 
     classes_matrix = np.array(classes_matrix)
 
     return classes_matrix
 
-def class_to_pixel (intClass):
+
+def class_to_pixel(intClass):
     """
     Converte una classe rappresentata da un intero
     in una lista di componenti colore.
- 
+    
     intClass: int, classe utilizzata come label nei
         dati di test o training.
     """
- 
+    
     if intClass == 0: # vetrino
         return [255, 255, 255, 255]
     elif intClass == 1: # strato corneo
@@ -252,19 +247,19 @@ def class_to_pixel (intClass):
 
 def classes_to_colimage(y, shape):
     """
-    Converte un array di classi y in un'immagine
+    Converte un'array di classi y in un'immagine
     da visualizzare con i colori originali.
-    
+
     y: numpy array, array unidimensionale di classi
         associate ai pixel di un'immagine.
     shape: iterable, la forma dell'immagine originale.
     """
 
     pixel_list = []
-    
+
     for pixel in y:
         pixel_list.append(class_to_pixel(pixel))
-
+        
     pixel_list = np.array(pixel_list)
     
     final_image = pixel_list.reshape(shape)
